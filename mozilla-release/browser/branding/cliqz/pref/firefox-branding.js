9175047dafd6cf0ebd832bf02d8c7d43f51b3d5a
Krzysztof Jan Modras <krzysztof@cliqz.com>
2015-08-26 10:10:46 +0200

    Autoconfig prefs

--- mozilla-release/browser/branding/cliqz/pref/firefox-branding.js
+++ mozilla-release/browser/branding/cliqz/pref/firefox-branding.js
@@ -26,3 +26,6 @@ pref("app.update.checkInstallTime.days", 63);
 // Number of usages of the web console or scratchpad.
 // If this is less than 5, then pasting code into the web console or scratchpad is disabled
 pref("devtools.selfxss.count", 0);
+
+pref("general.config.filename", "cliqz.cfg");
+pref("general.config.obscure_value", 0);
