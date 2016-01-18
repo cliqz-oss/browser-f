1f3861299c1f6cbafe42254964d7ea276f3b9da1
Lucian Corlaciu <lucian@cliqz.com>
2016-01-18 13:48:59 +0100

    update extension version

--- magic_build_and_package.sh
+++ magic_build_and_package.sh
@@ -95,9 +95,9 @@ echo '***** Building *****'
 
 #inject the repackaging
 if [[ "$CQZ_RELEASE_CHANNEL" == 'release' ]]; then
-  wget --output-document cliqz@cliqz.com.xpi http://cdn2.cliqz.com/update/browser/Cliqz.1.0.54.xpi
+  wget --output-document cliqz@cliqz.com.xpi http://cdn2.cliqz.com/update/browser/Cliqz.1.2.0.xpi
 else
-  wget --output-document cliqz@cliqz.com.xpi http://cdn2.cliqz.com/update/beta/Cliqz.1.2.0.1b0.xpi
+  wget --output-document cliqz@cliqz.com.xpi http://cdn2.cliqz.com/update/beta/Cliqz.1.2.0.1b1.xpi
 fi
 
 echo '***** Inject the repackaging *****'
