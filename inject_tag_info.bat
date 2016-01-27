e6418f1acf2a519d445606c18dc4c3bb84dd0279
Alexander Komarnitskiy <alexander@cliqz.com>
2016-01-27 15:04:33 +0100

    BROWSER-108: script for injecting tags into signed installer (will be used as one of build step, after signing)

---
+++ inject_tag_info.bat
@@ -0,0 +1,26 @@
+cd %CQZ_WORKSPACE%\mozilla-release\obj-firefox
+
+set ff_version=''
+for /F %%f in (..\browser\config\version.txt) do set ff_version=%%f
+set ff_exe=%ff_version%.en-US
+echo %ff_exe%
+if NOT "%lang%" == "" set ff_exe=%ff_version%.%lang%
+echo %ff_exe%
+echo %lang%
+
+set installer=dist\install\sea\CLIQZ-%ff_exe%.win32.installer.exe
+set tmp_installer=dist\install\sea\CLIQZ-%ff_exe%.win32.installer_tmp.exe
+
+rem Add tagged area to installer
+set GOROOT=c:\utils\omaha\go
+"%GOROOT%\bin\go.exe" run ..\..\cliqz-helpers\certificate_tag.go -set-superfluous-cert-tag=Gact2.0Omaha -padded-length=8206 -out %tmp_installer% %installer%
+del %installer%
+
+rem inject template information
+..\..\cliqz-helpers\ApplyTag.exe %tmp_installer% %installer% "brand=XXXXXXXXXX"
+del %tmp_installer%
+
+rem check installer still exist
+if not exist %installer% set ERRORLEVEL=1
+
+exit /b %ERRORLEVEL%
