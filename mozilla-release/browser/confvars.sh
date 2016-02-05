6571bedda202fa71362eaaf989f75f655af4b5f7
Max Breev <maxim@cliqz.com>
2016-02-05 17:35:16 +0100

    DB-312: Wrong application name used

--- mozilla-release/browser/confvars.sh
+++ mozilla-release/browser/confvars.sh
@@ -3,8 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=Firefox
-MOZ_APP_VENDOR=Mozilla
+MOZ_APP_BASENAME=CLIQZ
+MOZ_APP_VENDOR=Cliqz GmbH
 MOZ_UPDATER=1
 MOZ_PHOENIX=1
 
