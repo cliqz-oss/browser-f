d05d74cb15b1106e89e2571110c20364eb514bc9
Krzysztof Jan Modras <krzysztof.modras@gmail.com>
2016-02-24 14:00:59 +0100

    Build: Encapsulating shared part of magic scripts
    For developers convinience we can just:
    `source cliqz_env.sh`
    to have all environment variables set.

--- magic_upload_files.sh
+++ magic_upload_files.sh
@@ -4,81 +4,18 @@
 set -e
 set -x
 
-if [ -z "$LANG" ]; then
-  LANG='en-US'
-fi
-VERBOSE=false #TODO
+source cliqz_env.sh
 
-while [[ $# > 1 ]]
-do
-    key="$1"
-
-    case $key in
-        -lang|--language)
-        LANG="$2"
-        shift # past argument
-        ;;
-        -v|--verbose)
-        VERBOSE=true
-        ;;
-        *)
-        echo "unknown option $key"
-        exit
-        ;;
-    esac
-    shift # past argument or value
-done
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    export IS_LINUX=true
-    echo 'Linux OS detected'
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    export IS_MAC_OS=true
-    echo 'MAC OS detected'
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-    export IS_WIN=true
-    echo 'WINDOWS OS detected'
-elif [[ "$OSTYPE" == "msys" ]]; then
-    export IS_WIN=true
-    echo 'WINDOWS OS detected'
-else
-    echo 'Unknow OS -`$OSTYPE`'
-fi
-
-# TODO: Use MOZ_UPDATE_CHANNEL.
-if [ -z $CQZ_RELEASE_CHANNEL ]; then
-  CQZ_RELEASE_CHANNEL=release
-fi
-
-if [ $IS_WIN ]; then
-  MAKE=mozmake
-else
-  MAKE=make
-fi
+cd $OBJ_DIR
 
 echo "Starting build on with language $LANG and VERBOSE=$VERBOSE"
-
-export MOZCONFIG=browser/config/cliqz-release.mozconfig
-export CQZ_VERSION=$(awk -F "=" '/version/ {print $2}'\
-  ./repack/distribution/distribution.ini | head -n1)
-export CQZ_UI_LOCALE=`echo $LANG`
-export MOZ_AUTOMATION_UPLOAD=1
-export CQZ_BALROG_DOMAIN=balrog-admin.10e99.net
-export BALROG_PATH=../build-tools/scripts/updates
-export S3_BUCKET=repository.cliqz.com
-export S3_UPLOAD_PATH=`echo dist/$CQZ_RELEASE_CHANNEL/$CQZ_VERSION/${LANG:0:2}`
-
-OBJ_DIR=obj
-if [ $IS_MAC_OS ]; then
-  OBJ_DIR=$OBJ_DIR/i386
-fi
-cd $OBJ_DIR
 
 echo '***** Uploading MAR and package files *****'
 $MAKE upload
 
 echo '***** Genereting build_properties.json *****'
 $OLDPWD/mozilla-release/build/gen_build_properties.py
+
 cd $OLDPWD
 
 echo '***** Submiting to Balrog *****'
