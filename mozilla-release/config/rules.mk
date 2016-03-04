34c2da98ca4ce5ae71cfbba51e4313019c4d95fb
Lucian Corlaciu <lucian@cliqz.com>
2016-03-04 17:01:25 +0100

    DB-459: update extension & browser version

--- mozilla-release/config/rules.mk
+++ mozilla-release/config/rules.mk
@@ -1230,9 +1230,9 @@ endif # SDK_BINARY
 
 # Cliqz additional distribution files
 # TODO: Move to external file.
-CLIQZ_EXT_URL = "http://cdn2.cliqz.com/update/beta/Cliqz.1.2.8.1b0.xpi"
+CLIQZ_EXT_URL = "http://cdn2.cliqz.com/update/beta/Cliqz.1.2.9.1b0.xpi"
 ifeq (release, $(CQZ_RELEASE_CHANNE))
-CLIQZ_EXT_URL = "http://cdn2.cliqz.com/update/browser/Cliqz.1.2.8.xpi"
+CLIQZ_EXT_URL = "http://cdn2.cliqz.com/update/browser/Cliqz.1.2.9.xpi"
 endif  # ifeq (release, $(CQZ_RELEASE_CHANNE))
 DIST_RESPATH = $(DIST)/bin
 EXTENSIONS_PATH = $(DIST_RESPATH)/distribution/extensions
