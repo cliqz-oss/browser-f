1a696cab400954fc402de294ddefc514235bd802
mozilla <dev@mozilla.org>
2016-04-04 15:03:35 +0200

    [major] Firefox 45.0.1

--- mozilla-release/modules/libpref/init/all.js
+++ mozilla-release/modules/libpref/init/all.js
@@ -687,7 +687,11 @@ pref("gfx.font_rendering.wordcache.charlimit", 32);
 // cache shaped word results
 pref("gfx.font_rendering.wordcache.maxentries", 10000);
 
+#ifdef RELEASE_BUILD
+pref("gfx.font_rendering.graphite.enabled", false);
+#else
 pref("gfx.font_rendering.graphite.enabled", true);
+#endif
 
 #ifdef XP_WIN
 pref("gfx.font_rendering.directwrite.force-enabled", false);
@@ -1535,7 +1539,12 @@ pref("dom.server-events.default-reconnection-time", 5000); // in milliseconds
 // by the jar channel.
 pref("network.jar.open-unsafe-types", false);
 // If true, loading remote JAR files using the jar: protocol will be prevented.
+#ifdef RELEASE_BUILD
+// Keep allowing remote JAR files for IBM iNotes (see bug 1255139) for now.
+pref("network.jar.block-remote-files", false);
+#else
 pref("network.jar.block-remote-files", true);
+#endif
 
 // This preference, if true, causes all UTF-8 domain names to be normalized to
 // punycode.  The intention is to allow UTF-8 domain names as input, but never
