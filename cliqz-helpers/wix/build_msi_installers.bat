d574c01a95cfc235c6ea689912fdbff4ea106459
Alexander Komarnitskiy <alexander@cliqz.com>
2016-10-06 12:36:41 +0200

    DB-535: scripts for build MSI installers for 1.7.0

--- cliqz-helpers/wix/build_msi_installers.bat
+++ cliqz-helpers/wix/build_msi_installers.bat
@@ -10,5 +10,5 @@ candle cliqz_msi_en.wix.wxs
 light cliqz_msi_de.wix.wixobj
 light cliqz_msi_en.wix.wixobj
 
-rename cliqz_msi_en.wix.msi CLIQZ-1.6.1.en-US.win32.installer.msi
-rename cliqz_msi_de.wix.msi CLIQZ-1.6.1.de.win32.installer.msi
+rename cliqz_msi_en.wix.msi CLIQZ-1.7.0.en-US.win32.installer.msi
+rename cliqz_msi_de.wix.msi CLIQZ-1.7.0.de.win32.installer.msi
