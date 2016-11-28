f960bdaa377038dc3a467fc3ccce35ee1ee1732e
Leonid Rashkovsky <leonid@cliqz.com>
2016-11-28 09:19:45 +0100

    Changed echo to statement

--- build-helpers.groovy
+++ build-helpers.groovy
@@ -130,6 +130,7 @@ def withVagrant(String vagrantFilePath, String jenkinsFolderPath, Integer cpu, I
 def uploadExtensions(aws_credential_id, release_channel, cqz_build_id, cqz_extension_url, httpse_extension_url ) {
         cqz_version = sh(returnStdout: true, script: "awk -F '=' '/version/ {print \$2}' ./repack/distribution/distribution.ini | head -n1").trim()
         echo "versio  $cqz_version"
+        /*
         upload_path="s3://repository.cliqz.com/dist/$release_channel/$cqz_version/$cqz_build_id/cliqz@cliqz.com.xpi"
         httpse_upload_path="s3://repository.cliqz.com/dist/$release_channel/$cqz_version/$cqz_build_id/https-everywhere@cliqz.com.xpi"
 
@@ -145,6 +146,7 @@ def uploadExtensions(aws_credential_id, release_channel, cqz_build_id, cqz_exten
         }
         echo "url $cqz_extension_url to path  $upload_path"
         echo "url $httpse_extension_url path  $httpse_upload_path"
+        */
 }
 
 
