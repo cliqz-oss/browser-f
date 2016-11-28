07a34a6a50c01d85f8b8e3ffe8a981840e9a7323
Leonid Rashkovsky <leonid@cliqz.com>
2016-11-28 11:00:07 +0100

    Debug

--- build-helpers.groovy
+++ build-helpers.groovy
@@ -129,7 +129,8 @@ def withVagrant(String vagrantFilePath, String jenkinsFolderPath, Integer cpu, I
 
 @NonCPS
 def uploadExtensions(aws_credential_id, release_channel, cqz_build_id, cqz_extension_url, httpse_extension_url ) {
-        cqz_version = sh(returnStdout: true, script: 'awk -F \'=\' \'/version/ {print \$2}\' ./repack/distribution/distribution.ini | head -n1').trim()
+        //cqz_version = sh(returnStdout: true, script: 'awk -F \'=\' \'/version/ {print \$2}\' ./repack/distribution/distribution.ini | head -n1').trim()
+        cqz_version = sh(returnStdout: true, script='grep -oP "version=\K(.*)" ./repack/distribution/distribution.ini').trim()
         echo "version  ${cqz_version}"
         /*
         upload_path="s3://repository.cliqz.com/dist/${release_channel}/${cqz_version}/${cqz_build_id}/cliqz@cliqz.com.xpi"
