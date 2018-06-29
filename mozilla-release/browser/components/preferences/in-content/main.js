57c89a075ecb6d238a947d076a2f6c02096ace72
Alexander Komarnitskiy <alexander@cliqz.com>
2018-06-29 12:42:17 +0200

    DB-1765: make "When Cliqz start" settings partially works

--- mozilla-release/browser/components/preferences/in-content/main.js
+++ mozilla-release/browser/components/preferences/in-content/main.js
@@ -105,6 +105,12 @@ if (AppConstants.MOZ_DEV_EDITION) {
 Preferences.addAll([
   // Startup
   { id: "browser.startup.page", type: "int" },
+  { id: "browser.startup.homepage", type: "wstring" },
+
+  { id: "pref.browser.homepage.disable_button.current_page", type: "bool" },
+  { id: "pref.browser.homepage.disable_button.bookmark_page", type: "bool" },
+  { id: "pref.browser.homepage.disable_button.restore_default", type: "bool" },
+
   { id: "browser.privatebrowsing.autostart", type: "bool" },
 
   // Downloads
@@ -384,8 +390,10 @@ var gMainPane = {
       document.getElementById("warnOpenMany").hidden = true;
 
     // Startup pref
+#if 0
     setEventListener("browserRestoreSession", "command",
       gMainPane.onBrowserRestoreSessionChange);
+#endif
     gMainPane.updateBrowserStartupUI = gMainPane.updateBrowserStartupUI.bind(gMainPane);
     Preferences.get("browser.privatebrowsing.autostart").on("change",
       gMainPane.updateBrowserStartupUI);
@@ -759,6 +767,190 @@ var gMainPane = {
    *   restore)
    */
 
+  syncFromHomePref() {
+    let homePref = Preferences.get("browser.startup.homepage");
+
+    // Set the "Use Current Page(s)" button's text and enabled state.
+    this._updateUseCurrentButton();
+
+    function setInputDisabledStates(isControlled) {
+      let tabCount = this._getTabsForHomePage().length;
+
+      // Disable or enable the inputs based on if this is controlled by an extension.
+      document.querySelectorAll("#browserHomePage, .homepage-button")
+        .forEach((element) => {
+          let pref = element.getAttribute("preference");
+
+          let isDisabled = Preferences.get(pref).locked || isControlled;
+          if (pref == "pref.browser.disable_button.current_page") {
+            // Special case for current_page to disable it if tabCount is 0
+            isDisabled = isDisabled || tabCount < 1;
+          }
+
+          element.disabled = isDisabled;
+        });
+    }
+#if 0
+    if (homePref.locked) {
+      // An extension can't control these settings if they're locked.
+      hideControllingExtension(HOMEPAGE_OVERRIDE_KEY);
+      setInputDisabledStates.call(this, false);
+    } else {
+      // Asynchronously update the extension controlled UI.
+      handleControllingExtension(
+        PREF_SETTING_TYPE, HOMEPAGE_OVERRIDE_KEY, "extensionControlled.homepage_override2")
+        .then(setInputDisabledStates.bind(this));
+    }
+#endif
+    // If the pref is set to about:home or about:newtab, set the value to ""
+    // to show the placeholder text (about:home title) rather than
+    // exposing those URLs to users.
+    let defaultBranch = Services.prefs.getDefaultBranch("");
+    let defaultValue = defaultBranch.getComplexValue("browser.startup.homepage",
+      Ci.nsIPrefLocalizedString).data;
+    let currentValue = homePref.value.toLowerCase();
+    if (currentValue == "about:home" ||
+      (currentValue == defaultValue && currentValue == "about:newtab")) {
+      return "";
+    }
+
+    // If the pref is actually "", show about:blank.  The actual home page
+    // loading code treats them the same, and we don't want the placeholder text
+    // to be shown.
+    if (homePref.value == "")
+      return "about:blank";
+
+    // Otherwise, show the actual pref value.
+    return undefined;
+  },
+
+  syncToHomePref(value) {
+    // If the value is "", use about:home.
+    if (value == "")
+      return "about:home";
+
+    // Otherwise, use the actual textbox value.
+    return undefined;
+  },
+
+  /**
+   * Sets the home page to the current displayed page (or frontmost tab, if the
+   * most recent browser window contains multiple tabs), updating preference
+   * window UI to reflect this.
+   */
+  setHomePageToCurrent() {
+    let homePage = Preferences.get("browser.startup.homepage");
+    let tabs = this._getTabsForHomePage();
+    function getTabURI(t) {
+      return t.linkedBrowser.currentURI.spec;
+    }
+
+    // FIXME Bug 244192: using dangerous "|" joiner!
+    if (tabs.length) {
+      homePage.value = tabs.map(getTabURI).join("|");
+    }
+
+    Services.telemetry.scalarAdd("preferences.use_current_page", 1);
+  },
+
+  /**
+   * Displays a dialog in which the user can select a bookmark to use as home
+   * page.  If the user selects a bookmark, that bookmark's name is displayed in
+   * UI and the bookmark's address is stored to the home page preference.
+   */
+  setHomePageToBookmark() {
+    var rv = { urls: null, names: null };
+    gSubDialog.open("chrome://browser/content/preferences/selectBookmark.xul",
+      "resizable=yes, modal=yes", rv,
+      this._setHomePageToBookmarkClosed.bind(this, rv));
+    Services.telemetry.scalarAdd("preferences.use_bookmark", 1);
+  },
+
+  onBrowserHomePageChange() {
+    if (this.telemetryHomePageTimer) {
+      clearTimeout(this.telemetryHomePageTimer);
+    }
+    let browserHomePage = document.querySelector("#browserHomePage").value;
+    // The length of the home page URL string should be more then four,
+    // and it should contain at least one ".", for example, "https://mozilla.org".
+    if (browserHomePage.length > 4 && browserHomePage.includes(".")) {
+      this.telemetryHomePageTimer = setTimeout(() => {
+        let homePageNumber = browserHomePage.split("|").length;
+        Services.telemetry.scalarAdd("preferences.browser_home_page_change", 1);
+        Services.telemetry.keyedScalarAdd("preferences.browser_home_page_count", homePageNumber, 1);
+      }, 3000);
+    }
+  },
+
+  _setHomePageToBookmarkClosed(rv, aEvent) {
+    if (aEvent.detail.button != "accept")
+      return;
+    if (rv.urls && rv.names) {
+      var homePage = Preferences.get("browser.startup.homepage");
+
+      // XXX still using dangerous "|" joiner!
+      homePage.value = rv.urls.join("|");
+    }
+  },
+
+  /**
+   * Switches the "Use Current Page" button between its singular and plural
+   * forms.
+   */
+  async _updateUseCurrentButton() {
+    let useCurrent = document.getElementById("useCurrent");
+    let tabs = this._getTabsForHomePage();
+
+    const tabCount = tabs.length;
+
+    document.l10n.setAttributes(useCurrent, "use-current-pages", { tabCount });
+
+#if 0
+    // If the homepage is controlled by an extension then you can't use this.
+    if (await getControllingExtensionInfo(PREF_SETTING_TYPE, HOMEPAGE_OVERRIDE_KEY)) {
+      return;
+    }
+#endif
+    // In this case, the button's disabled state is set by preferences.xml.
+    let prefName = "pref.browser.homepage.disable_button.current_page";
+    if (Preferences.get(prefName).locked)
+      return;
+
+    useCurrent.disabled = tabCount < 1;
+  },
+
+  _getTabsForHomePage() {
+    var tabs = [];
+    var win = Services.wm.getMostRecentWindow("navigator:browser");
+
+    if (win && win.document.documentElement
+      .getAttribute("windowtype") == "navigator:browser") {
+      // We should only include visible & non-pinned tabs
+
+      tabs = win.gBrowser.visibleTabs.slice(win.gBrowser._numPinnedTabs);
+      tabs = tabs.filter(this.isNotAboutPreferences);
+      // XXX: Bug 1441637 - Fix tabbrowser to report tab.closing before it blurs it
+      tabs = tabs.filter(tab => !tab.closing);
+    }
+
+    return tabs;
+  },
+
+  /**
+   * Check to see if a tab is not about:preferences
+   */
+  isNotAboutPreferences(aElement, aIndex, aArray) {
+    return !aElement.linkedBrowser.currentURI.spec.startsWith("about:preferences");
+  },
+
+  /**
+   * Restores the default home page as the user's home page.
+   */
+  restoreDefaultHomePage() {
+    var homePage = Preferences.get("browser.startup.homepage");
+    homePage.value = homePage.defaultValue;
+  },
+
   /**
    * Utility function to enable/disable the button specified by aButtonID based
    * on the value of the Boolean preference specified by aPreferenceID.
@@ -799,19 +991,13 @@ var gMainPane = {
 */
   updateBrowserStartupUI() {
     const pbAutoStartPref = Preferences.get("browser.privatebrowsing.autostart");
-    const startupPref = Preferences.get("browser.startup.page");
-
-    let newValue;
-    let checkbox = document.getElementById("browserRestoreSession");
-    if (pbAutoStartPref.value || startupPref.locked) {
-      checkbox.setAttribute("disabled", "true");
-      newValue = false;
+    const restoreCheckbox = document.getElementById("restoreSessionCheckbox");
+    let checkbox = document.getElementById("restoreSessionCheckbox");
+    if (pbAutoStartPref.value) {
+      restoreCheckbox.setAttribute("disabled", "true");
+      restoreCheckbox.checked = false;
     } else {
       checkbox.removeAttribute("disabled");
-      newValue = startupPref.value === this.STARTUP_PREF_RESTORE_SESSION;
-    }
-    if (checkbox.checked !== newValue) {
-      checkbox.checked = newValue;
     }
   },
 
