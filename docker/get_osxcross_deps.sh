f63d4542abf6fa1b6f667ceb9b84dad02f08223a
Dhananjay Balan <dhananjay@cliqz.com>
2018-09-11 10:08:11 +0200

    Uses pre-built toolchains from mozilla to build.  - These can be accessed publically.

--- docker/get_osxcross_deps.sh
+++ docker/get_osxcross_deps.sh
@@ -2,10 +2,20 @@
 
 set -e -o pipefail
 
+if [[ -z $TOOLCHAIN_DATE ]]; then
+    TOOLCHAIN_DATE="2018-09-06"
+fi
+
 if [[ -z $OSX_SDK_PATH ]]; then
    OSX_SDK_PATH="s3://cliqz-ci/osxcross/MacOSX10.11.sdk.tar.bz2"
 fi
 
+# get toolchain bundles
+for bundle in "cbindgen.tar.xz" "cctools.tar.xz" "clang.tar.xz" "dmg.tar.xz" \
+                                "hfsplus-tools.tar.xz" "llvm-dsymutil.tar.xz" \
+                                "node.tar.xz" "rustc.tar.xz" "rust-size.tar.xz"; do
+    curl "https://s3.amazonaws.com/cliqz-ci/osxcross/toolchain/${TOOLCHAIN_DATE}/${bundle}" > ${bundle}
+done
+
 # get osx sdk bundle.
 aws s3 cp $OSX_SDK_PATH .
-
