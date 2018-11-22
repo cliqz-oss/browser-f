6af65b28faa8239577331e022bcee0ed3cd39900
Alex Catarineu <alex@cliqz.com>
2018-11-22 10:38:16 +0100

    Fix panelUI separator tweak

--- mozilla-release/browser/components/customizableui/content/panelUI.js
+++ mozilla-release/browser/components/customizableui/content/panelUI.js
@@ -106,7 +106,7 @@ const PanelUI = {
     Services.obs.notifyObservers(null, "appMenu-notifications-request", "refresh");
     if (Services.prefs.getBoolPref("extensions.cliqz.onion-mode", false)) {
       document.getElementById("appMenu-cliqz-connect-button").hidden = true;
-      document.getElementById("appMenu-first-separator").hidden = true;
+      document.getElementById("appMenu-tp-separator").hidden = true;
       document.getElementById("appMenu-new-window-button").hidden = true;
       document.getElementById("appMenu-private-window-button").hidden = true;
     }
