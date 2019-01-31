4ec823063685c1a3b3dca37dc68db2ce6d928de2
Trishul <trishul.goel@gmail.com>
2019-01-31 17:02:05 +0100

    DB-2020: Freshtab override issue

--- mozilla-release/browser/base/content/browser-siteIdentity.js
+++ mozilla-release/browser/base/content/browser-siteIdentity.js
@@ -490,7 +490,7 @@ var gIdentityHandler = {
 
     // CLIQZ: use brand name and logo if its system addon
     try {
-      isSystemAddon = this._pageExtensionPolicy.extension.addonData.signedState  == 3
+      isSystemAddon = (this._pageExtensionPolicy.extension.addonData.signedState  == 3 || this._pageExtensionPolicy.extension.addonData.builtIn)
     } catch(e) {
       isSystemAddon = false;
     }
