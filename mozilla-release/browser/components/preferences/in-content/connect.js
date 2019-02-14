534ca887d50f917c006fafa1ed31abad1e5e5df6
Trishul <trishul.goel@gmail.com>
2019-02-14 16:30:28 +0100

    DB-2044: removing legacy addon dependencies in the DB code

--- mozilla-release/browser/components/preferences/in-content/connect.js
+++ mozilla-release/browser/components/preferences/in-content/connect.js
@@ -1,5 +1,3 @@
-ChromeUtils.import("resource:///modules/CliqzResources.jsm");
-
 var gConnectPane = {
   _pane: null,
 
@@ -13,17 +11,5 @@ var gConnectPane = {
     this._pane = document.getElementById("paneConnect");
 
     document.getElementById('connectBox').parentNode.style.minWidth = '600px';
-
-    if (!CliqzResources.isWebExtensionAPI()) {
-      // Inject Iframe on init in case of legacy, legacy extension handles it with hashchange event
-      gConnectPane.loadFrame();
-    }
   },
-
-  loadFrame: function() {
-    const theUrl = CliqzResources.whatIstheURL('pairing/index.html');
-    const iframeEl = document.getElementById('theConnectFrame');
-    if (iframeEl && !iframeEl.getAttribute('src')) iframeEl.setAttribute('src', theUrl);
-  }
-
 };
