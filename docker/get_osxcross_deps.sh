7b5a464a9d8d05e32af2de5382bf80f9353614a0
joseph-cliqz <joseph@cliqz.com>
2019-05-16 13:28:54 +0200

    Modify and Add dependencies

--- docker/get_osxcross_deps.sh
+++ docker/get_osxcross_deps.sh
@@ -7,13 +7,14 @@ if [[ -z $TOOLCHAIN_DATE ]]; then
 fi
 
 if [[ -z $OSX_SDK_PATH ]]; then
-   OSX_SDK_PATH="s3://cliqz-ci/osxcross/MacOSX10.11.sdk.tar.bz2"
+   OSX_SDK_PATH="ftp://cliqznas/cliqz-browser-build-artifacts/MacOSX10.11.sdk.tar.bz2"
 fi
 
 # get toolchain bundles
-for bundle in "MacOSX10.11.sdk.tar.bz2" "cbindgen.tar.xz" "cctools.tar.xz" "clang.tar.xz" \
+for bundle in "nasm.tar.bz2" "cbindgen.tar.xz" "cctools.tar.xz" "clang.tar.xz" \
               "dmg.tar.xz" "hfsplus-tools.tar.xz" "llvm-dsymutil.tar.xz" \
               "node.tar.xz" "rustc.tar.xz" "rust-size.tar.xz"; do
     curl "https://repository.cliqz.com/dist/artifacts/mac/release/${CQZ_VERSION}/${bundle}" > ${bundle}
 done
+curl "${OSX_SDK_PATH}" > MacOSX10.11.sdk.tar.bz2
 
