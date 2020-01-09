798f9451cb88e1cc1e5233c9d63d75938d4788c9
Trishul <trishul.goel@gmail.com>
2020-01-09 16:53:44 +0100

    trimming connect.js

--- mozilla-release/browser/components/preferences/in-content/connect.js
+++ mozilla-release/browser/components/preferences/in-content/connect.js
@@ -1,17 +1,8 @@
 var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 var gConnectPane = {
-  _pane: null,
-
   init: function ()
   {
-    function setEventListener(aId, aEventType, aCallback)
-    {
-      document.getElementById(aId)
-              .addEventListener(aEventType, aCallback.bind(gConnectPane));
-    }
-    this._pane = document.getElementById("paneConnect");
-
     document.getElementById('connectBox').parentNode.style.minWidth = '600px';
   },
   loadFrame: function() {
