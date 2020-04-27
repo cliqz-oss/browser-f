cbc378c417afbe8b79020c6e2f5bd6af896bcf90
mozilla <dev@mozilla.org>
2020-04-27 15:54:12 +0200

    Fix crossbuild for Mac after merge with FF 76.0

--- docker/get_osxcross_deps.sh
+++ docker/get_osxcross_deps.sh
@@ -13,7 +13,8 @@ fi
 # get toolchain bundles
 for bundle in "nasm.tar.bz2" "cbindgen.tar.xz" "cctools.tar.xz" "clang.tar.xz" \
               "dmg.tar.xz" "hfsplus-tools.tar.xz" "llvm-dsymutil.tar.xz" \
-              "node.tar.xz" "rustc.tar.xz" "rust-size.tar.xz"; do
+              "node.tar.xz" "rustc.tar.xz" "rust-size.tar.xz" "lucetc.tar.xz" \
+              "wasi-sysroot.tar.xz"; do
     curl "https://repository.cliqz.com/dist/artifacts/mac/release/${CQZ_VERSION}/${bundle}" > ${bundle}
 done
 curl "${OSX_SDK_PATH}" > MacOSX10.11.sdk.tar.bz2
